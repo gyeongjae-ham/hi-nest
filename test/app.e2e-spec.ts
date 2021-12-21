454abd6f08529851ae85108d4567bf7e72b2d818
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-12-21 15:16:01 +0900

    Refactor movies e2e test.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    // 이 부분 없이 테스트를 돌리면 test가 제대로 실행이 안되는데 그 이유는 validationpipe가
+    // 입력값을 string에서 number로 바꿔주는 transform attribute를 제공하는게 테스트 서버에서는 없기 때문이다
+    // 따라서 테스트 할 때는 환경을 운영 서버와 같이 환경을 세팅해주고 테스트 해야 한다
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
     await app.init();
   });
 
@@ -27,7 +39,7 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({ title: 'Test', year: 2021, genres: ['test genres'] })
@@ -36,6 +48,38 @@ describe('AppController (e2e)', () => {
 
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2021,
+          genres: ['test genres'],
+          others: 'kkkkkkk im bad guy i will destroy ur server. kkkkkkkk',
+        })
+        .expect(400);
+    });
+
+    describe('/movies/:id', () => {
+      it('GET 200', () => {
+        return request(app.getHttpServer()).get('/movies/1').expect(200);
+      });
+      it('GET 404', () => {
+        return request(app.getHttpServer()).get('/movies/999').expect(404);
+      });
+
+      it('PATCH', () => {
+        return request(app.getHttpServer())
+          .patch('/movies/1')
+          .send({ title: 'Updated Test' })
+          .expect(200);
+      });
+
+      it('DELETE', () => {
+        return request(app.getHttpServer()).delete('/movies/1').expect(200);
+      });
     });
   });
 });
