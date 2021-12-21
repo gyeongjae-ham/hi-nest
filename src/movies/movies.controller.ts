7edde2168d6468f701e2647225d305bed8b837c8
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-12-21 10:39:16 +0900

    Practice http method decorater.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,23 +17,32 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made affter: ${searchingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieID: string) {
     return `This will delete a movie with the id: ${movieID}`;
   }
 
-  @Put('/:id')
-  patch(@Param('id') movieID: string) {
-    return `This will patch a movie with the id: ${movieID}`;
+  @Patch(':id')
+  patch(@Param('id') movieID: string, @Body() updateData) {
+    return {
+      updateMovie: movieID,
+      ...updateData,
+    };
   }
 }
